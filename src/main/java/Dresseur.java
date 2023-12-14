ecf3595228636d4794b2119d4d186f25c3a00dd6
Deyborah <deyborah.roger@gmail.com>
2023-12-14 16:59:50 +0100

    Dresseur en cours

--- src/main/java/Dresseur.java
+++ src/main/java/Dresseur.java
@@ -1,3 +1,35 @@
+import java.util.Random;
+import java.util.ArrayList;
+
 public class Dresseur {
+    protected String nom;
+    protected ArrayList<Dresseur> pokemon = new ArrayList<Dresseur>();
+
+    public Dresseur(
+            String nom
+    ) {
+        this.nom = nom;
+
+    }
+
+    /**
+     * Fonction qui sert à donner un chiffre aléatoire entre 0 et 6, plus ajout de plus 1 si n le nombre=0
+     *
+     */
+   public static void nombrePokemon() {
+
+        // génération d'un nombre >= 0 et < 6
+        Random r = new Random();
+        int n = r.nextInt(6);
+        if (n == 0) {
+            n = 1;
+        }
+        System.out.println(n);
+    }
+
+   /* public static void changerPokemon{
+         String listedresseur= ArrayList<Dresseur> pokemon;
+    }
+*/
 
 }
