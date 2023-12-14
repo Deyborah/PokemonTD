82b8b870eb11866a6b0e9dae83872b271e12ac3c
AntoineValenduc <ant.valenduc@gmail.com>
2023-12-14 17:00:42 +0100

    Fix : constant class especes

--- src/main/java/especes/Carapuce.java
+++ src/main/java/especes/Carapuce.java
@@ -2,12 +2,24 @@ package especes;
 
 public class Carapuce extends Pokemon {
 
+    /**
+     * Constante permettant d'insérer le nom de l'espèce dans le cas où
+     * l'utilisateur construit un pokémon sans lui donner de nom
+     */
     private static final String NOM = "Carapuce";
 
+    /**
+     * Constructeur sans paramètre de l'espèce Carapuce
+     * Surcharge de la propriété nom par le nom de l'espèce
+     */
     public Carapuce() {
         super.nom = NOM;
     }
 
+    /**
+     * Constructeur sans paramètre de l'espèce Carapuce
+     * Surcharge de la propriété nom par le nom donné par l'utilisateur
+     */
     public Carapuce(String nom){
         super.nom = nom;
     }
--- src/main/java/especes/Salameche.java
+++ src/main/java/especes/Salameche.java
@@ -2,12 +2,24 @@ package especes;
 
 public class Salameche extends Pokemon {
 
+    /**
+     * Constante permettant d'insérer le nom de l'espèce dans le cas où
+     * l'utilisateur construit un pokémon sans lui donner de nom
+     */
     private static final String NOM = "Salamèche";
 
+    /**
+     * Constructeur sans paramètre de l'espèce Salamèche
+     * Surcharge de la propriété nom par le nom de l'espèce
+     */
     public Salameche(){
         super.nom = NOM;
     }
 
+    /**
+     * Constructeur sans paramètre de l'espèce Salamèche
+     * Surcharge de la propriété nom par le nom donné par l'utilisateur
+     */
     protected Salameche(String nom) {
         super(nom);
     }
