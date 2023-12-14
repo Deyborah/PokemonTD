e7a827d9fe3b1be921ed383e5c33befc93a2ac04
AntoineValenduc <ant.valenduc@gmail.com>
2023-12-14 16:31:50 +0100

    add Pokemon, especes with Salameche + Carapuce + Bulbizarre

---
+++ src/main/java/especes/Bulbizarre.java
@@ -0,0 +1,14 @@
+package especes;
+
+public class Bulbizarre extends Pokemon {
+
+    private static final String NOM = "Bulbizarre";
+
+    public Bulbizarre() {
+        super.nom = NOM;
+    }
+
+    public Bulbizarre(String nom) {
+        super.nom = nom;
+    }
+}
---
+++ src/main/java/especes/Carapuce.java
@@ -0,0 +1,14 @@
+package especes;
+
+public class Carapuce extends Pokemon {
+
+    private static final String NOM = "Carapuce";
+
+    public Carapuce() {
+        super.nom = NOM;
+    }
+
+    public Carapuce(String nom){
+        super.nom = nom;
+    }
+}
